8a099c7e9edeb510ec14bd88bf4c9e357d5a3420
Mathieu Vallée <mathieu.vallee@protonmail.com>
2024-02-24 09:50:23 +0900

    before 8_12

--- build/runGPU_8_12.sh
+++ build/runGPU_8_12.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-#SBATCH --job-name=9_13_0
+#SBATCH --job-name=8_12
 #SBATCH --gres=gpu:1
 #SBATCH --qos=qos_gpu-t4
 #SBATCH --cpus-per-task=5
-#SBATCH --output=/data1/GPU_9_13_0.out
-#SBATCH --error=./errors/GPU_9_13_0.err
+#SBATCH --output=/data1/GPU_8_12_0
+#SBATCH --error=./errors/GPU_8_12_0
 #SBATCH --time=30:00:00
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=5
 #SBATCH --ntasks-per-node=1
-srun time ./8_12_test
+srun time ./8_12_0
